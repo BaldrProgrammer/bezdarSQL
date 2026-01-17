c07ddd135073cd690390e27455211959ed423df6
balddrAdmin <boliklevik@gmail.com>
2026-01-18 00:58:38 +0100

    raw select()

--- bsql/__init__.py
+++ bsql/__init__.py
@@ -1,29 +1,30 @@
-# from config import *
-# import psycopg2
-#
-# try:
-#     with psycopg2.connect(
-#             host=host,
-#             port=port,
-#             user=user,
-#             database=db_name,
-#             password=password,
-#     ) as connection:
-#         connection.autocommit = True
-#         with connection.cursor() as cursor:
-#             cursor.execute("""
-#                 select * from ads;
-#             """)
-#
-#             print(cursor.fetchall())
-#
-# except Exception as _e:
-#     print('error', _e)
-def select(tablename, **kwargs):
-    request = f'select {kwargs['value']} from {tablename} where '
-    filters = kwargs['filter_by']
-    for fil in filters:
-        request += f'and {fil}={filters[fil]}'
-    return request + ';'
+from .config import *
+import psycopg2
 
-print(select('ads', value='*', filter_by={'id': '1'}))
+def select(table, **kwargs):
+    request = f'select {kwargs['value']} from {table.__tablename__} where '
+    filters = kwargs['filter_by']
+    index = 0
+    for fil in filters:
+        index += 1
+        if index > 1:
+            request += f'and {fil}={filters[fil]} '
+        else:
+            request += f'{fil}={filters[fil]} '
+
+    try:
+        with psycopg2.connect(
+                host=host,
+                port=port,
+                user=user,
+                database=db_name,
+                password=password,
+        ) as connection:
+            connection.autocommit = True
+            with connection.cursor() as cursor:
+                cursor.execute(request + ';')
+
+                return cursor.fetchall()
+
+    except Exception as _e:
+        print('error', _e)
