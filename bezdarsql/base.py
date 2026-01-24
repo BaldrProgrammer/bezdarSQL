c0fd7e2ed878ff9a1033c0e2114f261c46c4d1c1
balddrAdmin <boliklevik@gmail.com>
2026-01-24 23:23:12 +0100

    Column __set_name__() method

--- bezdarsql/base.py
+++ bezdarsql/base.py
@@ -5,6 +5,12 @@ class Column:
     def __init__(self, **kwargs):
         self.autoincrement = kwargs.get('autoincrement')
 
+
+    def __set_name__(self, owner, name):
+        self.owner = owner
+        self.name = name
+
+
 @dataclasses.dataclass
 class Base:
     __tablename__ = None
